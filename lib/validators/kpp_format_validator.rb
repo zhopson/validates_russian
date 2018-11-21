c438c8167efe56f6401df0fdedcdb76e7fa870bb
Pavel Nikiforov <paul@yakutskenergo.ru>
2018-11-21 15:11:10 +0900

    New KPP format

--- lib/validators/kpp_format_validator.rb
+++ lib/validators/kpp_format_validator.rb
@@ -3,7 +3,7 @@ class KppFormatValidator < ValidatesRussian::Validator
   validates_using do |kpp|
     next false unless kpp.size == 9
     next false unless ValidatesRussian::REGION_NUMBERS.include?(kpp[0..1])
-    next false unless kpp =~ /^\d+$/
-    next false unless kpp[5..6] != '00'
+    next false unless kpp =~ /^\d{4}[A-Z0-9]{2}\d{3}$/
+    next false unless kpp[4..5] != '00'
   end
 end
